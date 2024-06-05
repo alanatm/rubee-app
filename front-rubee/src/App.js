4c4530b720f791d3b8a452da58ee0c1c6b7fe66c
alanatm <roalan96@gmail.com>
2024-06-05 16:53:02 -0600

    Se inicia Login

---
+++ front-rubee/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Inicio from './components/Inicio';
+import './components/css/styles.css';
+
+
+
+function App() {
+  return (
+    <Inicio></Inicio>
+  );
+}
+
+export default App;
---
+++ front-rubee/src/components/Inicio.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './css/styles.css'
+
+
+export default function Inicio() {
+  return (
+    <>
+    <div className='bg-body'></div>
+    <form action="" method="get" className='form-login'>
+        <h1 className='title-login'>Rubee App</h1>
+        <p>Ingresa tu correo:</p>
+        <input type="email" name="email-us" id="email-us" placeHolder='ejemplo@ejemplo.com' className='all-input input-login' />
+        <p>Ingresa tu contraseña:</p>
+        <input type="text" className='all-input input-login' placeholder='********' />
+        <a href="#" className='letra-chica margen-top'>¿Olvidaste tu contraseña?</a>
+        <button type="submit" className='margen-top'>Acceder</button>
+    </form>
+    </>
+  )
+}
+
